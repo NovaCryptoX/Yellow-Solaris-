89c8111576738578aa9516ceeef492c7fb7a57ab
NovaCryptoX <frankmorrison000000001@gmail.com>
2025-10-30 20:30:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// ✅ Proper Vite config for React Router + Netlify
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  server: {
-    historyApiFallback: true,
-  },
-});
+})
